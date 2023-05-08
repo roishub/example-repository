439ac19efdbe935e2dc45a3d42e391bf5888ca7a
rois <roisthomas088@gmail.com>
2023-05-08 10:33:59 +0530

    API testing

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify
+import requests
 
-app = Flask(__name__)
+# Make a GET request to the API endpoint
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
 
-@app.route('/hello')
-def hello():
-    return jsonify({'message': 'Hello, World!'})
+# Ensure the response is successful (status code 200)
+assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Ensure the response is in JSON format
+assert response.headers['Content-Type'] == 'application/json; charset=utf-8', "Expected response to be in JSON format"
+
+# Ensure the response contains the expected data
+expected_data = {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}
+assert response.json() == expected_data, f"Expected {expected_data} but got {response.json()}"
+
+print("All tests passed!")
+
